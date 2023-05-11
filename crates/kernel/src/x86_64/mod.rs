3c9e4b71a746d85d8613b3975551ce76cc7f49fb
Nils Mathieu <nils.mathieu.contact@gmail.com>
2023-05-11 07:07:39 +0200

    restore the old paging alocator

--- crates/kernel/src/x86_64/mod.rs
+++ crates/kernel/src/x86_64/mod.rs
@@ -3,12 +3,14 @@ mod boot;
 mod apic;
 mod interrupts;
 mod logger;
+mod paging;
 mod sys_info;
 mod tables;
 
 pub use self::apic::*;
 pub use self::interrupts::*;
 pub use self::logger::*;
+pub use self::paging::*;
 pub use self::sys_info::*;
 pub use self::tables::*;
 
